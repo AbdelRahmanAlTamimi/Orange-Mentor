fc5a7e351e217ee9c805bc5658528b9da5a42d5d
tamimi6030 <tamimi6030@gmail.com>
2024-10-06 18:12:59 +0300

    coach table done

--- back-end/app/Models/Coach.php
+++ back-end/app/Models/Coach.php
@@ -9,4 +9,20 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Coach extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['academy_id'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function academy() {
+        return $this->belongsTo(Academy::class);
+    }
+    public function submissions() {
+        return $this->hasMany(Submission::class);
+    }
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
 }
--- back-end/database/migrations/2024_10_06_144102_create_coaches_table.php
+++ back-end/database/migrations/2024_10_06_144102_create_coaches_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('coaches', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('academy_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
         });
